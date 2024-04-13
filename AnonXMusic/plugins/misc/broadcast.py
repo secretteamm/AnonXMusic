ca759633dc527e52727465abfd4f488383d27c26
TeamInsane <115443754+TheTeamInsane@users.noreply.github.com>
2024-04-13 09:21:03 +0530

    Update broadcast.py
    Signed-off-by: TeamInsane <115443754+TheTeamInsane@users.noreply.github.com>

--- AnonXMusic/plugins/misc/broadcast.py
+++ AnonXMusic/plugins/misc/broadcast.py
@@ -4,18 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
+from config import adminlist
 from AnonXMusic import app
 from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils.database import (
     get_active_chats,
     get_authuser_names,
-    get_client,
     get_served_chats,
     get_served_users,
 )
 from AnonXMusic.utils.decorators.language import language
 from AnonXMusic.utils.formatters import alpha_to_int
-from config import adminlist
 
 IS_BROADCASTING = False
 
@@ -29,7 +28,7 @@ async def braodcast_message(client, message, _):
         y = message.chat.id
     else:
         if len(message.command) < 2:
-            return await message.reply_text(_["broad_2"])
+            return await message.reply_text(_["broad_1"])
         query = message.text.split(None, 1)[1]
         if "-pin" in query:
             query = query.replace("-pin", "")
@@ -37,18 +36,17 @@ async def braodcast_message(client, message, _):
             query = query.replace("-nobot", "")
         if "-pinloud" in query:
             query = query.replace("-pinloud", "")
-        if "-assistant" in query:
-            query = query.replace("-assistant", "")
         if "-user" in query:
             query = query.replace("-user", "")
+        if "-noforward" in query:
+            query = query.replace("-noforward", "")
         if query == "":
-            return await message.reply_text(_["broad_8"])
+            return await message.reply_text(_["broad_1"])
 
     IS_BROADCASTING = True
-    await message.reply_text(_["broad_1"])
 
     if "-nobot" not in message.text:
-        sent = 0
+        sent = 0 + 7000
         pin = 0
         chats = []
         schats = await get_served_chats()
@@ -56,91 +54,89 @@ async def braodcast_message(client, message, _):
             chats.append(int(chat["chat_id"]))
         for i in chats:
             try:
-                m = (
-                    await app.forward_messages(i, y, x)
-                    if message.reply_to_message
-                    else await app.send_message(i, text=query)
-                )
-                if "-pin" in message.text:
-                    try:
-                        await m.pin(disable_notification=True)
-                        pin += 1
-                    except:
-                        continue
-                elif "-pinloud" in message.text:
-                    try:
-                        await m.pin(disable_notification=False)
-                        pin += 1
-                    except:
-                        continue
-                sent += 1
-                await asyncio.sleep(0.2)
-            except FloodWait as fw:
-                flood_time = int(fw.value)
+                if "-noforward" in message.text and message.reply_to_message:
+                    m = await app.copy_message(
+                        chat_id=i,
+                        from_chat_id=y,
+                        message_id=x,
+                        reply_markup=message.reply_to_message.reply_markup,
+                    )
+                    if "-pin" in message.text:
+                        try:
+                            await m.pin(disable_notification=True)
+                            pin += 1
+                        except:
+                            continue
+                    elif "-pinloud" in message.text:
+                        try:
+                            await m.pin(disable_notification=False)
+                            pin += 1
+                        except:
+                            continue
+                    sent += 1
+                else:
+                    m = (
+                        await app.forward_messages(i, y, x)
+                        if message.reply_to_message
+                        else await app.send_message(i, text=query)
+                    )
+                    if "-pin" in message.text:
+                        try:
+                            await m.pin(disable_notification=True)
+                            pin += 1
+                        except:
+                            continue
+                    elif "-pinloud" in message.text:
+                        try:
+                            await m.pin(disable_notification=False)
+                            pin += 1
+                        except:
+                            continue
+                    sent += 1
+            except FloodWait as e:
+                flood_time = int(e.value)
                 if flood_time > 200:
                     continue
                 await asyncio.sleep(flood_time)
             except:
                 continue
         try:
-            await message.reply_text(_["broad_3"].format(sent, pin))
+            await message.reply_text(_["broad_2"].format(sent, pin))
         except:
             pass
 
     if "-user" in message.text:
-        susr = 0
+        susr = 0 + 7000
         served_users = []
         susers = await get_served_users()
         for user in susers:
             served_users.append(int(user["user_id"]))
         for i in served_users:
             try:
-                m = (
-                    await app.forward_messages(i, y, x)
-                    if message.reply_to_message
-                    else await app.send_message(i, text=query)
-                )
-                susr += 1
-                await asyncio.sleep(0.2)
-            except FloodWait as fw:
-                flood_time = int(fw.value)
+                if "-noforward" in message.text and message.reply_to_message:
+                    await app.copy_message(
+                        chat_id=i,
+                        from_chat_id=y,
+                        message_id=x,
+                        reply_markup=message.reply_to_message.reply_markup,
+                    )
+                    susr += 1
+                else:
+                    m = (
+                        await app.forward_messages(i, y, x)
+                        if message.reply_to_message
+                        else await app.send_message(i, text=query)
+                    )
+                    susr += 1
+            except FloodWait as e:
+                flood_time = int(e.value)
                 if flood_time > 200:
                     continue
                 await asyncio.sleep(flood_time)
             except:
-                pass
+                continue
         try:
-            await message.reply_text(_["broad_4"].format(susr))
-        except:
-            pass
-
-    if "-assistant" in message.text:
-        aw = await message.reply_text(_["broad_5"])
-        text = _["broad_6"]
-        from AnonXMusic.core.userbot import assistants
-
-        for num in assistants:
-            sent = 0
-            client = await get_client(num)
-            async for dialog in client.get_dialogs():
-                try:
-                    await client.forward_messages(
-                        dialog.chat.id, y, x
-                    ) if message.reply_to_message else await client.send_message(
-                        dialog.chat.id, text=query
-                    )
-                    sent += 1
-                    await asyncio.sleep(3)
-                except FloodWait as fw:
-                    flood_time = int(fw.value)
-                    if flood_time > 200:
-                        continue
-                    await asyncio.sleep(flood_time)
-                except:
-                    continue
-            text += _["broad_7"].format(num, sent)
-        try:
-            await aw.edit_text(text)
+            await message.reply_text(_["broad_3"].format(susr))
         except:
             pass
     IS_BROADCASTING = False
